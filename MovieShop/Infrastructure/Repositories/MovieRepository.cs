84ff3d5189f50eeb7b696666e9f487f55415bacf
YuqiYou <you.269@osu.edu>
2022-07-19 12:08:05 -0700

    MovieShop

---
+++ MovieShop/Infrastructure/Repositories/MovieRepository.cs
@@ -0,0 +1,37 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.RepositoryContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class MovieRepository : IMovieRepository
+    {
+        public Movie GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Movie> GetTop30HighestRevenueMovies()
+        {   //Go to db and get the top movies
+            //EF Core or Dapper to connect to db
+            
+            var movies = new List<Movie> { 
+                new Movie{}
+            
+            };
+
+            return movies;
+
+
+        }
+
+        public List<Movie> GetTop30RatedMovies()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MovieShop/Infrastructure/Services/MovieService.cs
@@ -0,0 +1,29 @@
+﻿using ApplicationCore.Models;
+using ApplicationCore.ServiceContracts;
+using Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class MovieService : IMovieService
+    {
+        public List<MovieCardModel> GetTopRevenueMovies()
+        {
+            //commnunicate with repo
+            var movierepo = new MovieRepository();
+            var movies = movierepo.GetTop30HighestRevenueMovies();
+
+            var movieCards = new List<MovieCardModel>();
+            foreach (var movie in movies)
+            {
+                movieCards.Add(new MovieCardModel { Id = movie.Id, Title = movie.Title, PosterUrl = movie.PostUrl});
+            }
+
+            return movieCards;
+        }
+    }
+}
